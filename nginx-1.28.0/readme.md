bf5cf0bbb55f25b29a9c34e85c5450d4b0099f6b
silentlastdive <silentwish4586@gmail.com>
2026-01-21 17:20:55 +0800

    feat: 🎸 完成config配置上傳

---
+++ nginx-1.28.0/readme.md
@@ -0,0 +1,2 @@
+# config.js 配置
+![config配置](./internal-private-replaceholder.png "config配置")
---
+++ nginx-1.28.0/start_server.config.js
@@ -0,0 +1,48 @@
+const config = require('./config.js');
+const path = require('path');
+// 判斷方式：檢查是否有傳入 --env dev 參數
+const isDev = process.argv.includes('dev'); 
+const activeConfig = isDev ? config.local : config.server;
+/**
+ * 除了 apps，這個設定檔還可以包含其他頂級索引（Root keys），例如：deploy: 用於定義自動化部署（例如將程式碼從 GitHub 拉取到伺服器）。source_map_support: 全域開啟原始碼對照支援。
+ */
+
+// 使用 path.join 拼接
+const phpPath=activeConfig.phpPath;
+const nginxPath=activeConfig.nginxPath;
+const phpCgiExe = path.join(activeConfig.phpPath, 'php-cgi.exe');
+const phpListen = `127.0.0.1:${activeConfig.phpCgiPort}`;
+module.exports = {
+  apps : [
+    {
+      // 1. PHP-CGI 服務 (取代原本 .bat 裡的 php-cgi 部分)
+      name: 'php-cgi',
+      script: phpCgiExe,
+      args: `-b ${phpListen}`,
+      cwd: phpPath,
+      env: {
+        PHPRC: phpPath // 取代 set PHPRC
+      },
+      autorestart: true
+    },
+    {
+      // 2. Nginx 服務 (取代原本 .bat 裡的 nginx 部分)
+      name: 'nginx-server',
+      script: 'nginx.exe',
+      args: '-g "daemon off;"', // 必須加這行，PM2 才能後台監控它
+      cwd: nginxPath,
+      kill_timeout: 3000,
+      autorestart: true
+    },
+    {
+      // 3. Reverb 服務 (你原本的 Laravel Reverb)
+      name: 'reverb',
+      interpreter: 'php',
+      script: 'artisan',
+      args: 'reverb:start',
+      cwd: activeConfig.projectPath,
+      autorestart: true
+    }
+    
+  ]
+};
